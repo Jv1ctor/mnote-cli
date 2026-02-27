6ce4a57ec0d0d1338ecb32c29bd1885e0f824e72
Jv1ctor <j.rodriguesbarretos@gmail.com>
2026-02-27 16:59:31 -0300

    refactory(cli): adjust and instances service

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,17 @@
 #!/usr/bin/env bun
 
-import { cli } from "./cli/cli";
-import { Config } from "./config/config";
-
+import { cli } from "./cli/cli"
+import { Config } from "./config/config"
+import { VaultRepositoryFactory } from "./repositories/vault.repository.factory"
+import { VaultService } from "./services/vault.service"
 
 const run = async () => {
   const config = new Config()
   await config.execute()
 
-  cli(config)
+  const repositoryFactory = new VaultRepositoryFactory()
+  const service = new VaultService(repositoryFactory)
+  cli(service, config)
 }
 
 run()
