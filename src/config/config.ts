3adf0547f109b79b73e0a623a7e5809ad2551049
Jv1ctor <j.rodriguesbarretos@gmail.com>
2026-02-27 13:37:14 -0300

    feat(configs): add configs module for defined path for obsidian vaults

--- src/config/config.ts
+++ src/config/config.ts
@@ -1,25 +1,26 @@
-import { fileURLToPath, JSON5 } from "bun"
-import { dirname, join } from "path"
+import { join } from "path"
 import os from "os"
 import type { ConfigJsonSchema } from "./schema/config-json.schema"
 
-const __filename = fileURLToPath(import.meta.url)
-const __dirname = dirname(__filename)
+const root = process.cwd()
 const homeDir = os.homedir()
 
 export class Config {
-  private dir: string = Bun.env.NODE_ENV === "development" ? __dirname : homeDir
+  private dir: string = Bun.env.NODE_ENV === "development" ? root : homeDir
   private configDir = join(this.dir, ".mnote")
   private config = join(this.configDir, "config.json")
 
   async execute() {
-    await Bun.write(this.config, "{}")
+    const file = Bun.file(this.config)
+
+    if(!file)
+      await Bun.write(this.config, "{}")
   }
 
   async savePath(path: string) {
     try {
       const data = { path }
-      await Bun.write(this.config, JSON5.stringify(data) || "")
+      await Bun.write(this.config, JSON.stringify(data) || "")
     } catch (error) {
       console.log("Failed to write in config file")
     }
@@ -28,7 +29,7 @@ export class Config {
   async getPath() {
     try {
       const data = await Bun.file(this.config).text()
-      const config = JSON5.parse(data) as ConfigJsonSchema
+      const config = JSON.parse(data) as ConfigJsonSchema
       return config.path
     } catch (error) {
       console.log("Failed to read config file")
