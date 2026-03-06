3f2fe85eed4a89a65c966ee93c5205932db1a39d
Jv1ctor <j.rodriguesbarretos@gmail.com>
2026-03-06 09:25:50 -0300

    feat(vault): add update method in service

--- src/services/vault.service.ts
+++ src/services/vault.service.ts
@@ -1,6 +1,7 @@
 import type { Note } from "../entity/note.entity"
 import type { IVaultRepositoryFactory } from "../interfaces/vault-repository-factory.interface"
 import type { IVaultService } from "../interfaces/vault-service.interface"
+import type { OptionsUpdate } from "../types/options-update.types"
 
 export class VaultService implements IVaultService {
   constructor(private repoFactory: IVaultRepositoryFactory) {}
@@ -20,5 +21,12 @@ export class VaultService implements IVaultService {
     return repo.listAll(dirname)
   }
 
-  async update(path: string, note: Note): Promise<void> {}
+  async update(
+    path: string,
+    note: Note,
+    options: OptionsUpdate,
+  ): Promise<void> {
+    const repo = this.repoFactory.create(path)
+    await repo.update(note, options)
+  }
 }
