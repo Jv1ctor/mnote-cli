c7ffdbe6578d0c12c4665df744da547278b67074
Jv1ctor <j.rodriguesbarretos@gmail.com>
2026-03-06 09:26:57 -0300

    feat(mcp): add module mcp

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,7 @@
 
 import { cli } from "./cli/cli"
 import { Config } from "./config/config"
+import { mcp } from "./mcp/mcp"
 import { VaultRepositoryFactory } from "./repositories/vault.repository.factory"
 import { VaultService } from "./services/vault.service"
 
@@ -11,7 +12,11 @@ const run = async () => {
 
   const repositoryFactory = new VaultRepositoryFactory()
   const service = new VaultService(repositoryFactory)
-  cli(service, config)
+  // cli(service, config)
+  mcp(service, config).catch((error) => {
+    console.error("Fatal error in main():", error)
+    process.exit(1)
+  })
 }
 
 run()
