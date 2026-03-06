c28ec6ce3825b363c36e5fda75df0f33fcfd2ed0
Jv1ctor <j.rodriguesbarretos@gmail.com>
2026-03-06 09:24:41 -0300

    feat(vaults): add update method

--- src/interfaces/vault-repository.interface.ts
+++ src/interfaces/vault-repository.interface.ts
@@ -1,9 +1,10 @@
 import type { Note } from "../entity/note.entity";
+import type { OptionsUpdate } from "../types/options-update.types";
 
 
 export interface IVaultRepository {
   create(note: Note): Promise<void>
   read(path: string): Promise<string>
   listAll(path?: string): Promise<string[]>
-  update(note: Note): Promise<void>
+  update(note: Note, options?: OptionsUpdate): Promise<void>
 }
--- src/interfaces/vault-service.interface.ts
+++ src/interfaces/vault-service.interface.ts
@@ -1,8 +1,9 @@
 import type { Note } from "../entity/note.entity"
+import type { OptionsUpdate } from "../types/options-update.types"
 
 export interface IVaultService {
   create(path: string, note: Note): Promise<void>
   read(path: string, name: string): Promise<string>
   list(path: string, dirname?: string): Promise<string[]>
-  update(path: string, note: Note): Promise<void>
+  update(path: string, note: Note, options?: OptionsUpdate): Promise<void>
 }
